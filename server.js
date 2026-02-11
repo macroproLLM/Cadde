ee45d32c34f2b882a865748216635b887c98e58f
macroproLLM <info.macropro@gmail.com>
2026-02-11 16:19:09 +0300

    Implement screen sharing events
    Added screen sharing functionality with events for starting and stopping screen share.

--- server.js
+++ server.js
@@ -153,6 +153,15 @@ io.on('connection', (socket) => {
         socket.to(roomId).emit('voice-state-update', { id: socket.id, isSpeaking });
     });
 
+    // Screen Sharing Events
+    socket.on('screen-share-started', ({ roomId, channelName }) => {
+        socket.to(roomId).emit('user-screen-share-started', { id: socket.id, nickname: users[socket.id]?.nickname, channelName });
+    });
+
+    socket.on('screen-share-stopped', ({ roomId }) => {
+        socket.to(roomId).emit('user-screen-share-stopped', { id: socket.id });
+    });
+
     socket.on('disconnect', () => {
         for (const roomId in rooms) {
             const index = rooms[roomId].users.findIndex(u => u.id === socket.id);
